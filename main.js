cf6009a50bcd7c1573e83c78cfdb61a2295b4843
Lyneca <lukegithub@gmail.com>
2016-10-14 19:58:56 +1100

    reorganised, added /r/aww pics and nicknames

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 583, height: 152, frame:false})
+  mainWindow = new BrowserWindow({width: 583, height: 260, frame:false, resizable:false})
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
