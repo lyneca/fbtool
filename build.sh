6f2857890cbb964d71f2051769b67d0f2c25078e
Lyneca <lukegithub@gmail.com>
2016-10-16 17:29:29 +1100

    updated build.sh to output zipfiles for windows

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 electron-packager . fbtools --app-version $1 --arch ia32,x64 --platform win32,linux,darwin --asar --overwrite --out releases
 cd releases
 echo "Zipping..."
-for file in fbtools-*
+for file in fbtools-win32-*
+do
+echo "$file -> $file.zip"
+zip -qr "$file.zip" $file
+done
+for file in fbtools-linux-*
+do
+echo "$file -> $file.tar.gz"
+tar czf "$file.tar.gz" $file
+done
+for file in fbtools-darwin-*
 do
 echo "$file -> $file.tar.gz"
 tar czf "$file.tar.gz" $file
